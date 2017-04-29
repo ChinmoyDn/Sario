d6e4e6491b5e41010ddec1f86c8d984c2dc2f663
Chinmoy <chinmoy.debnath11@gmail.com>
2017-04-29 12:29:30 +0530

    WIP death of the spiders

--- js/main.js
+++ js/main.js
@@ -102,7 +102,8 @@ PlayState.create = function () {
   // create sound entities
   this.sfx = {
     jump: this.game.add.audio('sfx:jump'),
-    coin: this.game.add.audio('sfx:coin')
+    coin: this.game.add.audio('sfx:coin'),
+    stomp: this.game.add.audio('sfx:stomp')
   }
   this.game.add.image(0, 0, 'background');
   this._loadLevel(this.game.cache.getJSON('level:1'));
